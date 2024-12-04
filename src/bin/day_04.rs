79ba3619c61b2a93d5f346f2f5b9439a338e573a
Emily Hyland <hello@duien.com>
2024-12-03 22:36:59 -0800

    Day 4.1 terribly

---
+++ src/bin/day_04.rs
@@ -0,0 +1,79 @@
+const TEST_INPUT : &str = "\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+use std::fs;
+
+fn file_input() -> String {
+    let file_path = "../ruby/data/day_04.txt";
+    fs::read_to_string(file_path)
+        .expect("unable to read file")
+}
+
+fn main() {
+    dbg!(find_xmas(TEST_INPUT));
+    dbg!(find_xmas(file_input().as_str()));
+}
+
+fn find_xmas(input: &str) -> usize {
+    let char_grid : Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+    let grid_height = char_grid.len();
+    let grid_width = char_grid[0].len();
+    let mut exes = 0;
+    for (i, line) in char_grid.iter().enumerate() {
+        for (j, chr) in line.iter().enumerate() {
+            if *chr == 'X' {
+                let can_go_north = i >= 3;
+                let can_go_west = j >= 3;
+                let can_go_south = i < grid_height - 3;
+                let can_go_east = j < grid_width - 3;
+
+                if can_go_north &&
+                    char_grid[i-1][j] == 'M' &&
+                    char_grid[i-2][j] == 'A' &&
+                    char_grid[i-3][j] == 'S' { exes += 1; }
+                if can_go_south &&
+                    char_grid[i+1][j] == 'M' &&
+                    char_grid[i+2][j] == 'A' &&
+                    char_grid[i+3][j] == 'S' { exes += 1; }
+                if can_go_west &&
+                    char_grid[i][j-1] == 'M' &&
+                    char_grid[i][j-2] == 'A' &&
+                    char_grid[i][j-3] == 'S' { exes += 1; }
+                if can_go_east &&
+                    char_grid[i][j+1] == 'M' &&
+                    char_grid[i][j+2] == 'A' &&
+                    char_grid[i][j+3] == 'S' { exes += 1; }
+                if can_go_north && can_go_west &&
+                    char_grid[i-1][j-1] == 'M' &&
+                    char_grid[i-2][j-2] == 'A' &&
+                    char_grid[i-3][j-3] == 'S' { exes += 1; }
+                if can_go_north && can_go_east &&
+                    char_grid[i-1][j+1] == 'M' &&
+                    char_grid[i-2][j+2] == 'A' &&
+                    char_grid[i-3][j+3] == 'S' { exes += 1; }
+                if can_go_south && can_go_west &&
+                    char_grid[i+1][j-1] == 'M' &&
+                    char_grid[i+2][j-2] == 'A' &&
+                    char_grid[i+3][j-3] == 'S' { exes += 1; }
+                if can_go_south && can_go_east &&
+                    char_grid[i+1][j+1] == 'M' &&
+                    char_grid[i+2][j+2] == 'A' &&
+                    char_grid[i+3][j+3] == 'S' { exes += 1; }
+            }
+        }
+    }
+    
+    exes
+}
